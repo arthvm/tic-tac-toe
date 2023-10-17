69fe699773bb4b41fcb4b158b6f0a26eed7de689
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2023-10-17 11:58:01 -0300

    Add function to check empty spaces on the board
    This commit adds a function that returns an array with the indexes of
    the empty spaces on the board.

--- game.js
+++ game.js
@@ -1,10 +1,27 @@
+// Stores the logic related to the game itself
 const gameController = (function () {})();
+
+// Stores the logic that manages the board
 const gameBoard = (function () {
   let _board = new Array(9);
-  const getSlot = (indx) => _board[indx];
+
+  const getEmptyTiles = () => {
+    let _emptyTiles = new Array();
+    for (let i = 0; i < _board.length; i++) {
+      if (_board[i] == null) {
+        _emptyTiles.push(i);
+      }
+    }
+    return _emptyTiles;
+  };
+
+  return { getEmptyTiles };
 })();
+
+//Stores the logic that renders the game
 const displayController = (function () {})();
 
+//Player Factory
 function createPlayer(name, marker) {
   return { name, marker };
 }
